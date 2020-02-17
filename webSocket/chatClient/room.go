09e3e9e7d3327d43d58086a1083ca3612f3f9d08
underflow101 <ikarus125@gmail.com>
2020-02-17 15:43:30 +0900

    update
    Signed-off-by: underflow101 <ikarus125@gmail.com>

--- webSocket/chatClient/room.go
+++ webSocket/chatClient/room.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
 type room struct {
@@ -10,6 +12,15 @@ type room struct {
 	join    chan *client
 	leave   chan *client
 	clients map[*client]bool
+}
+
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+	}
 }
 
 func (r *room) run() {
