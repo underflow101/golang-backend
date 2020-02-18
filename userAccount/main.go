03c72bc45fa0b15cff488d219f998dfc41358f4f
underflow101 <ikarus125@gmail.com>
2020-02-18 17:31:32 +0900

    update
    Signed-off-by: underflow101 <ikarus125@gmail.com>

--- userAccount/main.go
+++ userAccount/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"flag"
 	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 	"sync"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 type templateHandler struct {
@@ -23,18 +27,25 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	var addr = flag.String("addr", ":8080", "The addr of the application.")
-	flag.Parse()
+	//var addr = flag.String("addr", ":8080", "The addr of the application.")
+	//flag.Parse()
+	gomniauth.SetSecurityKey("PUT YOUR AUTH KEY HERE")
+	gomniauth.WithProviders(
+		facebook.New("key", "secret", "http://localhost:8080/auth/callback/facebook"),
+		github.New("key", "secret", "http://localhost:8080/auth/callback/github"),
+		google.New("key", "secret", "http://localhost:8080/auth/callback/google"),
+	)
 	r := newRoom()
 	//http.Handle("/assets/", http.StripPrefix("/asstes", http.FileServer(http.Dir("/path/to/assets"))))
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandlerFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 
 	go r.run()
 
 	log.Println("Starting web server on", *addr)
-	if err := http.ListenAndServe(*addr, nil); err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
